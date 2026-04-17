c4cea56c890fe8bb577234a7a1fb7160e9ab35bb
isuri619 <isuriumayangana102@outlook.com>
2026-04-18 02:32:50 +0530

    API calls

---
+++ frontend/src/api/bookingApi.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8083/api/bookings";
+
+export const createBooking = (data) => axios.post(BASE_URL, data);
+
+export const getAllBookings = () => axios.get(BASE_URL);
+
+export const getUserBookings = (userId) =>
+  axios.get(`${BASE_URL}/user/${userId}`);
+
+export const getBookingById = (id) => axios.get(`${BASE_URL}/${id}`);
+
+export const updateBookingStatus = (id, status, reason) =>
+  axios.patch(`${BASE_URL}/${id}/status`, { status, reason });
+
+export const cancelBooking = (id, userId) =>
+  axios.patch(`${BASE_URL}/${id}/cancel`, null, { params: { userId } });
+
+export const deleteBooking = (id) => axios.delete(`${BASE_URL}/${id}`);

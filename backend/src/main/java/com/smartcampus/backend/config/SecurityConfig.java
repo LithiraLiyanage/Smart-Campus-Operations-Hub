8663a9e177b1925107eaccb350b048be9c7396d8
isuri619 <isuriumayangana102@outlook.com>
2026-04-18 01:20:50 +0530

    Add the database name and Disable Security Temporarily for Testing

--- backend/src/main/java/com/smartcampus/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/smartcampus/backend/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.smartcampus.backend.config;
+/*package com.smartcampus.backend.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -50,6 +50,29 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
+        return http.build();
+    }
+} */
+
+package com.smartcampus.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
         return http.build();
     }
 }
